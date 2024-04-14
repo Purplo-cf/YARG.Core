f21541fac32032df66a225e267220b4b12ce9130
EliteAsian <29520859+EliteAsian123@users.noreply.github.com>
2024-04-14 18:11:49 -0400

    Fixed note cloning when child notes have child notes

--- YARG.Core/Chart/Notes/Note.cs
+++ YARG.Core/Chart/Notes/Note.cs
@@ -215,7 +215,7 @@ namespace YARG.Core.Chart
             var newNote = CloneWithoutChildNotes();
             foreach (var child in _childNotes)
             {
-                newNote.AddChildNote(child.CloneNote());
+                newNote.AddChildNote(child.Clone());
             }
 
             return newNote;
